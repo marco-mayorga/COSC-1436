1a62d46ee591655a642dd803520bd1c2b9a7e1de
Marco <marco.mayorga@gmail.com>
2022-09-01 12:16:12 -0500

    update code to ask all info from user

--- Chapter1/HelloWorld.cpp
+++ Chapter1/HelloWorld.cpp
@@ -10,15 +10,53 @@ user information, your information should output (20 pts). The bold information 
 to name your file HelloWorld.cpp .  (no space between Hello and World)  (30 pts)*/
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	cout << "\nName: Marco Mayorga\n\n";
-	cout << "Date: August 2021\n\n";
-	cout << "Course: COSC 1436\n\n";
-	cout << "Major: Computer Science\n\n";
-	cout << "GPA: Not Known at this time.";
-	return 0;
-};
+	// 1. Sets user variables
+	string name = "";
+	string date = "";
+	string course = "";
+	string major = "";
+	double gpa = 0.0;
+
+	// 2. Ask for the users Information
+	// 3. Store info
+
+	// 2a. Ask user for name
+	cout << "What is your name? ";
+	// 3a. Store name info
+	getline(cin, name);
+
+	// 2b. Ask user for the date
+	cout << "What is the date? ";
+	// 3b. Store date info
+	getline(cin, date);
+
+	// 2c. Ask user for Course name
+	cout << "What is the Course? ";
+	// 3c. Store Course info
+	getline(cin, course);
+
+	// 2d. Ask user for their Major
+	cout << "What is your Major? ";
+	// 3d. Store Major info
+	getline(cin, major);
+
+	// 2e. Ask for their GPA
+	cout << "What is your GPA? ";
+	// 3e. Store GPA info
+	cin >> gpa;
+
+	// 4. No calculations
+
+	// 5. Display
+	cout << "\nName: " << name << endl;
+	cout << "Date: " << date << endl;
+	cout << "Course: " << course << endl;
+	cout << "Major: " << major << endl;
+	cout << "GPA: " << gpa << endl;
+}
