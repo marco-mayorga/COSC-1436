b5f60d980580613a3ee74066f28751691d79da92
Marco <marco.mayorga@gmail.com>
2022-09-01 09:36:29 -0500

    File generated for first assignment

---
+++ Chapter1/HelloWorld.cpp
@@ -0,0 +1,24 @@
+/*Marco Mayorga
+
+COSC-1436
+
+August 30, 2022
+
+For your assignment, you will create a C++ program that will output the information about yourself or your customers
+(users). A sample output is provided below.  Be sure your program outputs exactly what is shown below except for the
+user information, your information should output (20 pts). The bold information will be unique to each person. Be sure
+to name your file HelloWorld.cpp .  (no space between Hello and World)  (30 pts)*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\nName: Marco Mayorga\n\n";
+	cout << "Date: August 2021\n\n";
+	cout << "Course: COSC 1436\n\n";
+	cout << "Major: Computer Science\n\n";
+	cout << "GPA: Not Known at this time.";
+	return 0;
+};
