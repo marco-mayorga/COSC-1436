2e3166a4a40702b043d1b54735efa27186190786
Marco <marco.mayorga@gmail.com>
2022-09-13 10:50:13 -0500

    update template.py to new version

--- template.py
+++ template.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 from datetime import date
 import os
-# Creates C++ files with the top of the file filled out like prof wants.
-# This is the template of how she wants it
+import platform
+
+# This is the template of how Prof wants the cpp file
 """
 /*Name
-
 Course
-
 Date
-
 Assignment Description*/
 """
 
@@ -17,70 +15,108 @@ Assignment Description*/
 
 
 def main():
-    # clears screen
-    os.system('clear')
 
-    # file name
-    file_name = input("What is the file name: ")
+    # Moves to directory py script is stored
+    os.chdir(os.getcwd())
 
-    # My name
-    my_name = "Marco Mayorga"
-
-    # What course this is
-    course = "COSC-1436"
-
-    # Gets todays date
-    today = date.today()
-    today_formatted = today.strftime("%B %d, %Y")
-
-    # assignment description
-    description = input("Copy and paste assignment description from d2l: ")
-
-    # asks user what chapter folder to put file in
-    while True:
-        try:
-            chapter_num = int(
-                input("\nWhat chapter do I save the file to? (1-7): "))
-            break
-        except:
-            os.system('clear')
-            print("\nTry a number from 1-7: ")
-
-    if chapter_num in range(1, 8):
+    # Checks if Mac OS
+    if (platform.system() == "Darwin"):
+        # clears screen
         os.system('clear')
-        print("File created!")
 
-    # changes to chapter folder
-    save_where(chapter_num)
+        # Asks for file name
+        file_name = input("What is the file name: ")
 
-    # Creates cpp file
-    with open(file_name + ".cpp", "w") as cppfile:
-        # writes my info to the file
-        cppfile.write(
-            f"/*{my_name}\n\n{course}\n\n{today_formatted}\n\n{description}*/\n\n")
-        # makes a basic cpp function under my info
-        cppfile.write(
-            "#include <iostream>\n\nusing namespace std;\n\nint main()\n{\n\tcout << \"Hello World\";\n\treturn 0;\n}")
+        # User name
+        my_name = input("What is your name: ")
 
-# chooses directory to move into
+        # What course this is
+        course = "COSC-1436"
 
+        # Gets todays date
+        today = date.today()
+        today_formatted = today.strftime("%B %d, %Y")
 
-def save_where(num):
+        # Assignment description
+        description = input("Copy and paste assignment description from d2l: ")
 
-    if num == 1:
-        os.chdir("Chapter1")
-    elif num == 2:
-        os.chdir("Chapter2")
-    elif num == 3:
-        os.chdir("Chapter3")
-    elif num == 4:
-        os.chdir("Chapter4")
-    elif num == 5:
-        os.chdir("Chapter5")
-    elif num == 6:
-        os.chdir("Chapter6")
-    elif num == 7:
-        os.chdir("Chapter7")
+        while True:
+            try:
+                where_save_file = input(
+                    "Do you have a folder you want to save it to?: (y or n): ")
+                if where_save_file == "y":
+                    os.system("clear")
+                    path = input("Give me PathName to desired location: ")
+                    os.chdir(path)
+                    os.system("clear")
+                    break
+                elif where_save_file == "n":
+                    os.system("clear")
+                    print("Saving to current location")
+                    break
+            except:
+                print("Try y or n")
+
+        # Creates cpp file
+        with open(file_name + ".cpp", "w") as cppfile:
+            # writes my info to the file
+            cppfile.write(
+                f"/*{my_name}\n\n{course}\n\n{today_formatted}\n\n{description}*/\n\n")
+            # makes a basic cpp function under my info
+            cppfile.write(
+                "#include <iostream>\n\nusing namespace std;\n\nint main()\n{\n\treturn 0;\n}")
+        print("File Creation succesful.")
+
+    # Checks if Windows
+    elif (platform.system() == "Windows"):
+
+        # clears console
+        os.system("cls")
+
+        # file name
+        file_name = input("What is the file name: ")
+
+        # My name
+        my_name = "Marco Mayorga"
+
+        # What course this is
+        course = "COSC-1436"
+
+        # Gets todays date
+        today = date.today()
+        today_formatted = today.strftime("%B %d, %Y")
+
+        # assignment description
+        description = input("Copy and paste assignment description from d2l: ")
+
+        # Where to Save File
+        while True:
+            try:
+                where_save_file = input(
+                    "Do you have a folder you want to save it to?: (y or n): ")
+                if where_save_file == "y":
+                    os.system("cls")
+                    path = input("Give me PathName to desired location: ")
+                    os.chdir(path)
+                    os.system("cls")
+                    break
+                elif where_save_file == "n":
+                    # Saves file to current dir
+                    os.system("cls")
+                    print("Saving File to current location")
+                    break
+            except:
+                print("Try y or n")
+
+        # Creates cpp file
+        with open(file_name + ".cpp", "w") as cppfile:
+            # writes my info to the file
+            cppfile.write(
+                f"/*{my_name}\n\n{course}\n\n{today_formatted}\n\n{description}*/\n\n")
+            # makes a basic cpp function under my info
+            cppfile.write(
+                "#include <iostream>\n\nusing namespace std;\n\nint main()\n{\n\treturn 0;\n}")
+        print("File Creation succesful.")
 
 
 # calls the main function and runs it.
