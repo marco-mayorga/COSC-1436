fb00ef186729d7128bec8f44d9e04ae3e4e56f88
Marco <marco.mayorga@gmail.com>
2022-09-22 14:26:23 -0500

    Update to add built in comments

--- template.py
+++ template.py
@@ -44,13 +44,13 @@ def main():
             try:
                 where_save_file = input(
                     "Do you have a folder you want to save it to?: (y or n): ")
-                if where_save_file == "y":
+                if where_save_file == "y" or where_save_file == "Y" or where_save_file == "yes" or where_save_file == "Yes":
                     os.system("clear")
                     path = input("Give me PathName to desired location: ")
                     os.chdir(path)
                     os.system("clear")
                     break
-                elif where_save_file == "n":
+                elif where_save_file == "n" or where_save_file == "N" or where_save_file == "no" or where_save_file == "No":
                     os.system("clear")
                     print("Saving to current location")
                     break
@@ -58,13 +58,16 @@ def main():
                 print("Try y or n")
 
         # Creates cpp file
-        with open(file_name + ".cpp", "w") as cppfile:
+        with open(file_name + ".cpp", "a") as cppfile:
             # writes my info to the file
             cppfile.write(
                 f"/*{my_name}\n\n{course}\n\n{today_formatted}\n\n{description}*/\n\n")
             # makes a basic cpp function under my info
+            cppfile.write("#include <iostream>\n\nusing namespace std;")
             cppfile.write(
-                "#include <iostream>\n\nusing namespace std;\n\nint main()\n{\n\treturn 0;\n}")
+                "\n\nint main()\n{\n\t//1.Variables\n\t//2.Ask User for 'type in here'\n\t//3.Store user answer")
+            cppfile.write(
+                "\n\t//4.Calculations and logic\n\t//5.Display\n\treturn 0; \n}")
         print("File Creation succesful.")
 
     # Checks if Windows
@@ -94,28 +97,30 @@ def main():
             try:
                 where_save_file = input(
                     "Do you have a folder you want to save it to?: (y or n): ")
-                if where_save_file == "y":
-                    os.system("cls")
+                if where_save_file == "y" or where_save_file == "Y" or where_save_file == "yes" or where_save_file == "Yes":
+                    os.system("clear")
                     path = input("Give me PathName to desired location: ")
                     os.chdir(path)
-                    os.system("cls")
+                    os.system("clear")
                     break
-                elif where_save_file == "n":
-                    # Saves file to current dir
-                    os.system("cls")
-                    print("Saving File to current location")
+                elif where_save_file == "n" or where_save_file == "N" or where_save_file == "no" or where_save_file == "No":
+                    os.system("clear")
+                    print("Saving to current location")
                     break
             except:
                 print("Try y or n")
 
         # Creates cpp file
-        with open(file_name + ".cpp", "w") as cppfile:
+        with open(file_name + ".cpp", "a") as cppfile:
             # writes my info to the file
             cppfile.write(
                 f"/*{my_name}\n\n{course}\n\n{today_formatted}\n\n{description}*/\n\n")
             # makes a basic cpp function under my info
+            cppfile.write("#include <iostream>\n\nusing namespace std;")
             cppfile.write(
-                "#include <iostream>\n\nusing namespace std;\n\nint main()\n{\n\treturn 0;\n}")
+                "\n\nint main()\n{\n\t//1.Variables\n\t//2.Ask User for 'type in here'\n\t//3.Store user answer")
+            cppfile.write(
+                "\n\t//4.Calculations and logic\n\t//5.Display\n\treturn 0; \n}")
         print("File Creation succesful.")
 
 
